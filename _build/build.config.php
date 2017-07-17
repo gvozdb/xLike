9d0e47301cbb3c115836d77d358459523ed3e46b
gvozdb <i@owl.kz>
2017-07-17 17:45:50 +0600

    1.0.1-beta

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME', 'xLike');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 define('PKG_NAME_SHORT', 'xl');
 
-define('PKG_VERSION', '1.0.0');
+define('PKG_VERSION', '1.0.1');
 define('PKG_RELEASE', 'beta');
 define('PKG_AUTO_INSTALL', true);
 define('PKG_NAMESPACE_PATH', '{core_path}components/' . PKG_NAME_LOWER . '/');
@@ -33,8 +33,8 @@ define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
 // define build options
 define('BUILD_MENU_UPDATE', true);
 define('BUILD_SETTING_UPDATE', false);
+define('BUILD_CHUNK_UPDATE', false);
 
-define('BUILD_CHUNK_UPDATE', true);
 define('BUILD_SNIPPET_UPDATE', true);
 define('BUILD_PLUGIN_UPDATE', true);
 define('BUILD_EVENT_UPDATE', true);
